d5bdb251adc35911f8dc49a42abe360290ea6977
vgonzcam <59830206+vgonzcam@users.noreply.github.com>
2025-01-09 13:57:32 +0100

    Commit

--- main.tf
+++ main.tf
@@ -7,8 +7,16 @@ terraform {
       name = "vgonzcam-btp-backend"
     }
   }
+
+  required_providers {
+    btp = {
+      source  = "SAP/btp"
+      version = "1.8.0"
+    }
+  }
 }
 
 output "hello" {
   value = "Hello from main.tf! (with Mario)"
 }
+
